9125a4deae3d4b4547baabbd96c6584ca161abba
KozecM <kozecm@onid.oregonstate.edu>
2016-05-15 19:46:32 -0700

    added assig6

---
+++ Assig6.js
@@ -0,0 +1,45 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/',function(req,res){
+  res.render('home');
+});
+
+app.get('/post_get', function(req, res){
+  var stuff = []
+  var type = {};
+  type.status = "GET";
+  for (var f in req.query){
+    stuff.push({'name': f, 'value':req.query[f]});
+  }
+  var context = {};
+  context.Listdata = stuff;
+  res.render('post_get', context);
+});
+
+app.post(/post_get)
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
