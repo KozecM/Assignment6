ee942475c2f9e8120d6a368f0d8ed791609ac86b
KozecM <kozecm@onid.oregonstate.edu>
2016-05-15 19:53:59 -0700

    fix

--- Assig6.js
+++ Assig6.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
